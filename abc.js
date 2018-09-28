c1b88522c8d2e072e957f41d0ce7269138c0cc8b
sripat99 <42346535+sripat99@users.noreply.github.com>
2018-09-28 23:21:20 +0530

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,18 @@
+books = {
+  contents: "chapters",
+  study: "knowledge",
+  members : [
+  {
+    name:"steph",
+    rno:"123",
+    address:"home"
+  },
+
+    {
+      name:"lebron",
+      rno:"345",
+      address:"hom"
+    }
+  ]
+
+}
